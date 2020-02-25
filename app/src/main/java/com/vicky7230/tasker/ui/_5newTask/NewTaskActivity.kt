586c1bace29023fd81121982a621dc595b919df1
Vipin Kumar <vipin@mildlyclassic.com>
2020-02-25 16:50:37 +0530

    date and time selection finished

--- app/src/main/java/com/vicky7230/tasker/ui/_5newTask/NewTaskActivity.kt
+++ app/src/main/java/com/vicky7230/tasker/ui/_5newTask/NewTaskActivity.kt
@@ -2,21 +2,14 @@ package com.vicky7230.tasker.ui._5newTask
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.applikeysolutions.cosmocalendar.model.Day
-import com.applikeysolutions.cosmocalendar.utils.SelectionType
 import com.vicky7230.tasker.R
 import com.vicky7230.tasker.ui._0base.BaseActivity
 import com.vicky7230.tasker.utils.AnimUtils
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_new_task.*
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
 import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEventListener
 import net.yslibrary.android.keyboardvisibilityevent.util.UIUtil
@@ -82,106 +75,65 @@ class NewTaskActivity : BaseActivity() {
         //KeyboardUtils.showSoftInput(task_edit_text, this)
 
         val calendarInstance = Calendar.getInstance()
-        //val currentMonth = SimpleDateFormat("MMMM yyyy", Locale.getDefault()).format(calendarInstance.time)
-        val currentDay = Day(calendarInstance)
-        //calendarInstance.set(2020, 1, 23)
 
-        calendar_view.selectionType = SelectionType.SINGLE
-        calendar_view.calendarOrientation = LinearLayoutManager.HORIZONTAL
-        calendar_view.currentDayIconRes = R.drawable.calendar_dot
-        calendar_view.currentDaySelectedIconRes = R.drawable.calendar_dot_selected
-        calendar_view.currentDayTextColor = Color.parseColor("#006CFF")// NOT WORKING
-        calendar_view.weekendDayTextColor = Color.parseColor("#252A31")
-        calendar_view.selectedDayBackgroundColor = Color.parseColor("#006CFF")
+        val dateFormatter = SimpleDateFormat("d LLL YYYY", Locale.ENGLISH)
+        val formattedDate = dateFormatter.format(calendarInstance.time)
+        task_date.text = formattedDate
 
-        calendar_view.isShowDaysOfWeek = true
-        calendar_view.isShowDaysOfWeekTitle = false
-        calendar_view.nextMonthIconRes = R.drawable.ic_chevron_right
-        calendar_view.previousMonthIconRes = R.drawable.ic_chevron_left
+        val timeFormatter = SimpleDateFormat("h:m a", Locale.ENGLISH)
+        val formattedTime = timeFormatter.format(calendarInstance.time)
+        task_time.text = formattedTime
 
-        calendar_view.selectionManager.toggleDay(currentDay)
-
-        /*val disabledDaysSet: MutableSet<Long> = HashSet()
-
-        var job: Job? = null
-        calendar_view.setOnMonthChangeListener { month: Month ->
-            Timber.d(month.monthName)
-            job?.cancel()
-            job = lifecycleScope.launch {
-                for (day in month.days) {
-                    if (day.calendar.time.before(currentDay.calendar.time)) {
-                        if (!day.isDisabled) {
-                            disabledDaysSet.add(day.calendar.timeInMillis)
-                        }
-                    }
-                }
-                //withContext(Dispatchers.Main) {
-                calendar_view.disabledDays = disabledDaysSet
-                //}
-            }
-        }*/
+        calendar_view.minDate = calendarInstance.time.time
+        calendar_view.date = calendarInstance.time.time
+        calendar_view.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            calendarInstance.set(year, month, dayOfMonth)
+            val selectedDate = dateFormatter.format(calendarInstance.time)
+            task_date.text = selectedDate
+        }
 
         calendar_button.setOnClickListener { view: View ->
             UIUtil.hideKeyboard(this)
-            //calendar_view_container.visibility = View.VISIBLE
-            //AnimUtils.slideView(calendar_view_container, 0, calendar_view_container.layoutParams.height)
-            //view.isSelected = true
-
-            lifecycleScope.launch {
-                delay(50)
-                view.isSelected = true
-                calendar_view_container.visibility = View.VISIBLE
-                time_view_container.visibility = View.GONE
-                AnimUtils.slideView(calendar_view_container, 0, calendarViewContainerHeight)
-
-            }
+            view.isSelected = true
+            calendar_view_container.visibility = View.VISIBLE
+            time_view_container.visibility = View.GONE
+            AnimUtils.slideView(calendar_view_container, 0, calendarViewContainerHeight)
         }
 
         date_cancel_button.setOnClickListener {
-            calendar_view_container.visibility = View.GONE
+            //calendar_view_container.visibility = View.GONE
+            AnimUtils.slideView(calendar_view_container, calendarViewContainerHeight, 0)
         }
 
         date_done_button.setOnClickListener {
-            if (calendar_view.selectedDates.size > 0) {
-
-                if (calendar_view.selectedDates[0][Calendar.DAY_OF_MONTH] >= calendarInstance[Calendar.DAY_OF_MONTH] &&
-                    calendar_view.selectedDates[0][Calendar.MONTH] >= calendarInstance[Calendar.MONTH] &&
-                    calendar_view.selectedDates[0][Calendar.YEAR] >= calendarInstance[Calendar.YEAR]
-                ) {
-                    val formatter = SimpleDateFormat("d LLL YYYY", Locale.ENGLISH)
-                    val formattedDate = formatter.format(calendar_view.selectedDates[0].time)
-                    task_date.text = formattedDate
-
-                    calendar_view_container.visibility = View.GONE
-                } else {
-                    showError("Selected Date should be today or ahead.")
-                }
-            } else {
-                showError("Please select a date.")
-            }
+            //calendar_view_container.visibility = View.GONE
+            AnimUtils.slideView(calendar_view_container, calendarViewContainerHeight, 0)
         }
 
-        time_view.setIs24HourView(true)
+        time_view.setOnTimeChangedListener { view, hourOfDay, minute ->
+            calendarInstance.set(Calendar.HOUR_OF_DAY, hourOfDay)
+            calendarInstance.set(Calendar.MINUTE, minute)
+
+            task_time.text = timeFormatter.format(calendarInstance.time)
+        }
 
         time_button.setOnClickListener { view: View ->
             UIUtil.hideKeyboard(this)
-            /*time_view_container.visibility = View.VISIBLE
-            val height = time_view_container.layoutParams.height
-            time_view_container.layoutParams.height = 0
-            AnimUtils.slideView(calendar_view_container, 0, height)*/
-            lifecycleScope.launch {
-                delay(50)
-                view.isSelected = true
-                time_view_container.visibility = View.VISIBLE
-                calendar_view_container.visibility = View.GONE
-                Timber.d("Height of time widget : ${time_view_container.height}")
-                AnimUtils.slideView(time_view_container, 0, timeViewContainerHeight)
-            }
+            view.isSelected = true
+            time_view_container.visibility = View.VISIBLE
+            calendar_view_container.visibility = View.GONE
+            Timber.d("Height of time widget : ${time_view_container.height}")
+            AnimUtils.slideView(time_view_container, 0, timeViewContainerHeight)
         }
 
-        lifecycleScope.launch {
-            delay(2000)
-            Timber.d("Height of time widget : ${time_view_container.height}")
+        time_cancel_button.setOnClickListener {
+            //time_view_container.visibility = View.GONE
+            AnimUtils.slideView(time_view_container, timeViewContainerHeight, 0)
+        }
+
+        time_done_button.setOnClickListener {
+            //time_view_container.visibility = View.GONE
+            AnimUtils.slideView(time_view_container, timeViewContainerHeight, 0)
         }
     }
 }
