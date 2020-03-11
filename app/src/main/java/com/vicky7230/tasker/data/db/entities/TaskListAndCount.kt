c222018052b6d0d4ae271505346da42702486a65
Vipin Kumar <vicky7230@gmail.com>
2020-03-11 16:08:12 +0530

    WIP

--- app/src/main/java/com/vicky7230/tasker/data/db/entities/TaskListAndCount.kt
+++ app/src/main/java/com/vicky7230/tasker/data/db/entities/TaskListAndCount.kt
@@ -5,17 +5,12 @@ import androidx.room.ColumnInfo
 data class TaskListAndCount(
     @ColumnInfo(name = "id")
     var id: Long,
-
     @ColumnInfo(name = "list_slack")//this is the server identifier of this tasklist
     var listSlack: String,
-
     @ColumnInfo(name = "name")
     var name: String,
-
     @ColumnInfo(name = "color")
     var color: String,
-
     @ColumnInfo(name = "task_count")
     var taskCount: Int
-
 )
