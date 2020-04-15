e9f6ad66243912a67b8e8e491269638882228230
Vipin Kumar <vicky7230@gmail.com>
2020-04-15 15:34:37 +0530

    remove add butoon from UI of TasksActivity

--- app/src/main/java/com/vicky7230/tasker/ui/_6taskList/TasksActivity.kt
+++ app/src/main/java/com/vicky7230/tasker/ui/_6taskList/TasksActivity.kt
@@ -7,19 +7,13 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.transition.ChangeBounds
-import android.view.Gravity
 import android.view.View
-import android.widget.AdapterView
-import androidx.appcompat.widget.ListPopupWindow
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.vicky7230.tasker.R
 import com.vicky7230.tasker.ui._0base.BaseActivity
-import com.vicky7230.tasker.ui._4home.PopupAdapter
-import com.vicky7230.tasker.ui._5newTask.NewTaskActivity
-import com.vicky7230.tasker.utils.AnimUtilskt
 import com.vicky7230.tasker.utils.AppConstants
 import com.vicky7230.tasker.widget.ElasticDragDismissFrameLayout
 import dagger.android.AndroidInjection
@@ -28,7 +22,7 @@ import timber.log.Timber
 import javax.inject.Inject
 
 
-class TasksActivity : BaseActivity(), AdapterView.OnItemClickListener {
+class TasksActivity : BaseActivity() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -39,8 +33,6 @@ class TasksActivity : BaseActivity(), AdapterView.OnItemClickListener {
     private lateinit var tasksViewModel: TasksViewModel
     private lateinit var listName: String
     private lateinit var chromeFader: ElasticDragDismissFrameLayout.SystemChromeFader
-    private lateinit var listPopupWindow: ListPopupWindow
-    private var options = arrayListOf("Task", "List")
 
     companion object {
 
@@ -84,24 +76,12 @@ class TasksActivity : BaseActivity(), AdapterView.OnItemClickListener {
     @SuppressLint("SetTextI18n")
     private fun init() {
 
-        createPopup()
-
-        add_button_2.setOnClickListener {
-            AnimUtilskt.rotateFab(
-                add_button_2,
-                45.0F,
-                ContextCompat.getColor(this, R.color.colorBlue),
-                ContextCompat.getColor(this, R.color.colorWhite)
-            )
-            listPopupWindow.show()
-        }
 
         chromeFader = object : ElasticDragDismissFrameLayout.SystemChromeFader(this) {
             override fun onDragDismissed() {
                 Timber.d("onDragDismissed")
                 list_name.visibility = View.GONE
                 edit_list_name.visibility = View.GONE
-                add_button_2.visibility = View.GONE
                 supportFinishAfterTransition()
             }
         }
@@ -146,41 +126,9 @@ class TasksActivity : BaseActivity(), AdapterView.OnItemClickListener {
         }
     }
 
-    private fun createPopup() {
-
-        listPopupWindow = ListPopupWindow(this)
-        listPopupWindow.setAdapter(PopupAdapter(options))
-        listPopupWindow.anchorView = add_button_2
-        listPopupWindow.setDropDownGravity(Gravity.END)
-        listPopupWindow.width = resources.getDimension(R.dimen.popup_width).toInt()
-        listPopupWindow.height = ListPopupWindow.WRAP_CONTENT
-        listPopupWindow.verticalOffset =
-            resources.getDimension(R.dimen.popup_vertical_offset).toInt()
-        listPopupWindow.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.curved_bg))
-        listPopupWindow.isModal = true
-        listPopupWindow.setOnItemClickListener(this)
-        listPopupWindow.setOnDismissListener {
-            AnimUtilskt.rotateFab(
-                add_button_2,
-                0.0F,
-                ContextCompat.getColor(this, R.color.colorWhite),
-                ContextCompat.getColor(this, R.color.colorBlue)
-            )
-        }
-    }
-
-    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        if (position == 0) {
-            startActivity(NewTaskActivity.getStartIntent(this))
-        }
-
-        listPopupWindow.dismiss()
-    }
-
     override fun onBackPressed() {
         list_name.visibility = View.GONE
         edit_list_name.visibility = View.GONE
-        add_button_2.visibility = View.GONE
         supportFinishAfterTransition()
     }
 }
