c13ba2e5dbbab6415b366d4124f91a8d81447a8c
Vipin Kumar <vicky7230@gmail.com>
2020-04-15 15:42:48 +0530

    animations are working

--- app/src/main/java/com/vicky7230/tasker/data/Config.kt
+++ app/src/main/java/com/vicky7230/tasker/data/Config.kt
@@ -3,6 +3,6 @@ package com.vicky7230.tasker.data
 object Config {
     //TODO
     //val BASE_URL = "http://ec2-35-154-149-144.ap-south-1.compute.amazonaws.com/tasker/"
-    val BASE_URL = "http://192.168.0.101/tasker/"
+    val BASE_URL = "http://192.168.0.104/tasker/"
     //val BASE_URL = "https://tasker7230.000webhostapp.com/Tasker/"
 }
