fe867c6496c0bf9b9c35c11cda0179a53a3d987f
Vipin Kumar <vicky7230@gmail.com>
2020-05-06 19:49:47 +0530

    work on finished and deleted tasks

---
+++ app/src/main/java/com/vicky7230/tasker/ui/_7finishedDeleted/FinishedDeletedTasksActivity.kt
@@ -0,0 +1,13 @@
+package com.vicky7230.tasker.ui._7finishedDeleted
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.vicky7230.tasker.R
+
+class FinishedDeletedTasksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finished_deleted_tasks)
+    }
+}
