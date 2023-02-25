1c6f2e523d730ab43180225fbd51b3efcc8d9e0f
hamid.f@bugloos.com <>
2023-02-25 18:34:04 +0330

    change ReminderBroadcastReceiver class to fix some issues + fix variable naming + fix flag in multi android version + declare string value in strings.xml + create NotificationConfig.kt for define request code and Id + add post notification to AndroidManifest.xml

--- app/src/main/java/com/vicky7230/tasker/receiver/ReminderBroadcastReceiver.kt
+++ app/src/main/java/com/vicky7230/tasker/receiver/ReminderBroadcastReceiver.kt
@@ -5,38 +5,43 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.vicky7230.tasker.R
+import com.vicky7230.tasker.receiver.NotificationConfig.NOTIFICATION_ID
+import com.vicky7230.tasker.receiver.NotificationConfig.PENDING_INTENT_REQUEST_CODE
 import com.vicky7230.tasker.ui._1splash.SplashActivity
 import com.vicky7230.tasker.ui._5newTask.NewTaskActivity
 
 class ReminderBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-
-        val i = Intent(context, SplashActivity::class.java)
-
-        val pi = PendingIntent.getActivity(
+        val mIntent = Intent(context, SplashActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
             context,
-            101,
-            i,
-            PendingIntent.FLAG_UPDATE_CURRENT
+            PENDING_INTENT_REQUEST_CODE,
+            mIntent,
+            getPendingIntentGetActivityFlag()
         )
-
-        val builder = NotificationCompat.Builder(context, "Notify_Task")
-            .setSmallIcon(R.drawable.ic_tasker_notification)
-            .setContentTitle("Pending task")
-            .setContentText(intent.getStringExtra(NewTaskActivity.EXTRAS_TASK))
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setDefaults(Notification.DEFAULT_LIGHTS)
-            .setDefaults(Notification.DEFAULT_SOUND)
-            .setDefaults(Notification.DEFAULT_VIBRATE)
-            .setContentIntent(pi)
-            .setAutoCancel(true)
-
-        val nm = NotificationManagerCompat.from(context)
-        nm.notify(200, builder.build())
+        val notificationBuilder =
+            NotificationCompat.Builder(context, context.getString(R.string.notify_task))
+                .setSmallIcon(R.drawable.ic_tasker_notification)
+                .setContentTitle(context.getString(R.string.pending_task))
+                .setContentText(intent.getStringExtra(NewTaskActivity.EXTRAS_TASK))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(Notification.DEFAULT_LIGHTS)
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+        val notificationManagerCompat = NotificationManagerCompat.from(context)
+        notificationManagerCompat.notify(NOTIFICATION_ID, notificationBuilder.build())
     }
 
+    private fun getPendingIntentGetActivityFlag(): Int =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        else
+            PendingIntent.FLAG_UPDATE_CURRENT
 }
